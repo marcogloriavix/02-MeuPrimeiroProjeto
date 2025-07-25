0c52f80d089e8c4310638c96cf748a988287761b
marcogloriavix <marcogloriavix@gmail.com>
2025-07-25 16:03:02 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+console.log("Olá Mundo . Estou de Volta !!!");
+
+//************Comandos novos  - NO TERMINAL   ********************
+// node --watch src/index.js
+// Fica vigiando as alterações e replicanco no terminal, ao salvar o arquivo. 
+//
+//Para desligar o modo WATCH, é clicar Ctrl+C  ;
+// 
+
+// ***** PROJETO NODE  *****
+// npm init -y         inicia um projeto node - GERENCIADOR DE PACOTES NODE
+// node nome_arquivo    roda arquivo javascript - ATENçÃO AO caminho
+// ******
+
+// ******      Comandos de navegação de diretórios:    *********
+// del*     deleta arquivos
+//  ls      lista arquivos no diretório
+//  cls     (clear)  limpa terminal
+//  cd      (change directory) muda de diretório
+//  cd..     recua uma pasta (directório)
+//  mkdir    (make directory) cria um diretório novo
+//  code     (vscode) abre o editor de código do vscode
+//  code .    abre nova aba do Vscode
+
+//********************************************** */
+
+
